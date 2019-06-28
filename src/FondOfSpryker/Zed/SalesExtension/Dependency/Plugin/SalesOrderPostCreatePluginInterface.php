6f4c5f9a8c2195645bf2c6d55d641015066c183d
Jozsef Geng <gengjozsef86@gmail.com>
2019-06-28 12:33:37 +0200

    add Plugin Interface SalesOrderPostCreatePluginInterface

---
+++ src/FondOfSpryker/Zed/SalesExtension/Dependency/Plugin/SalesOrderPostCreatePluginInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FondOfSpryker\Zed\SalesExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\SaveOrderTransfer;
+
+interface SalesOrderPostCreatePluginInterface
+{
+    /**
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\SaveOrderTransfer $saveOrderTransfer
+     */
+    public function execute(OrderTransfer $orderTransfer, SaveOrderTransfer $saveOrderTransfer): void;
+}
