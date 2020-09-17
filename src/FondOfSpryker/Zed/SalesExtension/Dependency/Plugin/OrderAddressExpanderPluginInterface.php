2659079f8779e96736c0a9367d07fe9c7eddcbc2
Chris <christopher.egger@fondof.de>
2020-09-17 10:26:48 +0200

    Add new interface for order address expander

---
+++ src/FondOfSpryker/Zed/SalesExtension/Dependency/Plugin/OrderAddressExpanderPluginInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FondOfSpryker\Zed\SalesExtension\Dependency\Plugin;
+
+use Generated\Shared\Transfer\AddressTransfer;
+
+interface OrderAddressExpanderPluginInterface
+{
+    /**
+     * Specification:
+     *  - Manipulates AddressTransfer on order item level before storing in Persistence
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\AddressTransfer $addressTransfer
+     * @return \Generated\Shared\Transfer\AddressTransfer
+     */
+    public function expand(AddressTransfer $addressTransfer): AddressTransfer;
+}
